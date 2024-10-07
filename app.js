41cc1bf8795138f6e92bb33d5f46bda4a16fbcf5
PotioneQQ <127747695+PotioneQQ@users.noreply.github.com>
2024-10-07 23:12:04 +0200

    first commit

---
+++ app.js
@@ -0,0 +1,21 @@
+const express = require('express')
+const fetchGameData = require('./fetchGameData.js')
+const app = express()
+const port = 3000
+const url = 'localhost'
+const urlPort = 2999
+const path = '/liveclientdata/allgamedata'
+const rootCas = require('ssl-root-cas').create();
+require('https').globalAgent.options.ca = rootCas;
+rootCas.addFile("riotgames.pem")
+
+var data = fetchGameData.getData(url, urlPort, path)
+
+app.get('/', (req, res) => {
+    console.log(data)
+    console.log("a")
+    res.send(data)
+})
+app.listen(port, () => {
+    console.log(`port ${port}`)
+})
---
+++ fetchGameData.js
@@ -0,0 +1,23 @@
+const getData = (url, port, path) => {
+    var https = require('node:https')
+    var tls = require('node:tls')
+    var fs = require('node:fs')
+    var process = require('node:process')
+    const rootCas = require('ssl-root-cas').create();
+    require('https').globalAgent.options.ca = rootCas;
+    rootCas.addFile("riotgames.pem")
+    var lolApiPath = (`https://${url}:${port}${path}`)
+    var data
+    console.log(lolApiPath)
+    https.get(lolApiPath, (res) => {
+      console.log('statusCode:', res.statusCode)
+      console.log('headers:', res.headers)
+      res.on('data', (d) => {
+        data = process.stdout.write(d)
+        return d;
+      })
+    })
+    return d;
+}
+
+module.exports = { getData }

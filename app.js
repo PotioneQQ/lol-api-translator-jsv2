80c792262405de93541c20d2b25ac6ca01b3541d
PotioneQQ <127747695+PotioneQQ@users.noreply.github.com>
2024-10-08 11:26:00 +0200

    Update app.js
    test change of declaration in require fetchGameData.js

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const fetchGameData = require('./fetchGameData.js')
+const { getData } = require('./fetchGameData.js')
 const app = express()
 const port = 3000
 const url = 'localhost'
